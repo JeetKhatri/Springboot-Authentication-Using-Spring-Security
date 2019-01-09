f07afb7e070f7c6328f887a578047cedae76e5ec
JeetKhatri <jeet.g.khatri@gmail.com>
2019-01-09 17:13:55 +0530

    3rd way - with roles

--- src/main/java/com/springboot/config/SpringSecurityConfiguration.java
+++ src/main/java/com/springboot/config/SpringSecurityConfiguration.java
@@ -1,15 +1,26 @@
 package com.springboot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
-	
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	// for third way with roles
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER");
+	} 
+
+	/*
+	// for 1st and 2nd way
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
-	
+	*/
 }
